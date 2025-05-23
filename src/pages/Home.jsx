66ce1ab6b0c202bb640f261c27714027e30e6e24
DangetiPardheevKrishna <pardheevkrishna@gmail.com>
2025-05-23 14:46:01 +0530

    added readme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,17 @@
 import { Box, VStack } from "@chakra-ui/react";
-import React, { Suspense, lazy } from "react";
-
-// Lazy load components
-const Dashboard = lazy(() => import("../components/Dashboard"));
-const Items = lazy(() => import("../components/Items"));
-const OtherCosts = lazy(() => import("../components/OtherCosts"));
-const Navbar = lazy(() => import("../components/Navbar"));
+import Dashboard from "../components/Dashboard";
+import Items from "../components/Items";
+import OtherCosts from "../components/OtherCosts";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <Box bg="gray.50" minH="100vh">
       <VStack spacing={8} align="stretch">
-        <Suspense fallback={<div>Loading Navbar...</div>}>
-          <Navbar />
-        </Suspense>
-
-        <Suspense fallback={<div>Loading Dashboard...</div>}>
-          <Dashboard />
-        </Suspense>
-
-        <Suspense fallback={<div>Loading Items...</div>}>
-          <Items />
-        </Suspense>
-
-        <Suspense fallback={<div>Loading Other Costs...</div>}>
-          <OtherCosts />
-        </Suspense>
+        <Navbar />
+        <Dashboard />
+        <Items />
+        <OtherCosts />
       </VStack>
     </Box>
   );
